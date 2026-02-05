029895a401d145a63aac8b5609cb3a69da6363f1
hiro-14 <hhatori14@icloud.com>
2026-02-06 00:59:27 +0900

    DAY9 クリックでメッセージを書き換え

--- DAY9/script.js
+++ DAY9/script.js
@@ -1,1 +1,10 @@
 console.log("JavaScriptが読み込まれました");
+
+const button = document.getElementById("btn");
+console.log(button);
+const message = document.getElementById("message");
+
+button.addEventListener("click", function () { 
+  console.log("ボタンがクリックされました");
+  message.textContent = "こんにちは! JavaScript成功! 🎉";
+});
